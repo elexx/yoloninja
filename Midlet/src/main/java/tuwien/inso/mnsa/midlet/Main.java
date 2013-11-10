c1d977986ef29752cfa269111d405bdb9a725810
Michael Borkowski <michael@borkowski.at>
2013-11-10 13:32:26 +0100

    wip with talking to the card (see midlet.Main)

--- Midlet/src/main/java/tuwien/inso/mnsa/midlet/Main.java
+++ Midlet/src/main/java/tuwien/inso/mnsa/midlet/Main.java
@@ -9,6 +9,7 @@ import javax.microedition.contactless.DiscoveryManager;
 import javax.microedition.contactless.TargetListener;
 import javax.microedition.contactless.TargetProperties;
 import javax.microedition.contactless.TargetType;
+import javax.microedition.contactless.sc.ISO14443Connection;
 import javax.microedition.io.CommConnection;
 import javax.microedition.io.Connector;
 import javax.microedition.lcdui.Command;
@@ -41,7 +42,9 @@ public class Main extends MIDlet implements CommandListener, TargetListener {
 
 		try {
 			DiscoveryManager dm = DiscoveryManager.getInstance();
+			dm.addTargetListener(this, TargetType.ISO14443_CARD);
 			dm.addTargetListener(this, TargetType.NDEF_TAG);
+			dm.addTargetListener(this, TargetType.RFID_TAG);
 		} catch (ContactlessException ce) {
 			log.println("Unable to register TargetListener: " + ce.toString());
 		}
@@ -67,6 +70,69 @@ public class Main extends MIDlet implements CommandListener, TargetListener {
 
 		TargetProperties tmp = targetProperties[0];
 		log.println("UID read: " + tmp.getUid());
+		boolean isIso = false;
+
+		for (int i = 0; i < tmp.getTargetTypes().length; i++) {
+			TargetType type = tmp.getTargetTypes()[i];
+			log.println("target type: " + type.toString());
+
+			if (type.equals(TargetType.ISO14443_CARD))
+				isIso = true;
+		}
+
+		for (int i = 0; i < tmp.getConnectionNames().length; i++) {
+			log.println("conn name: " + tmp.getConnectionNames()[i].getName());
+		}
+
+		if (!isIso)
+			return;
+
+		ISO14443Connection connection;
+		try {
+			Class firstConnection = tmp.getConnectionNames()[0];
+			String url = tmp.getUrl(firstConnection);
+			log.println("openning connection " + url + "...");
+			connection = (ISO14443Connection) Connector.open(url);
+		} catch (IOException e) {
+			log.println("could not create iso14443 connection: " + e.toString());
+			return;
+		}
+
+		log.println("opened connection");
+
+		//		byte[] SELECT = { 0x00, // CLA Class 
+		//		(byte) 0xA4, // INS Instruction 
+		//		0x04, // P1 Parameter 1
+		//		0x00, // P2 Parameter 2
+		//		(byte) 0xA0, // Length
+		//		0x63, 0x64, 0x63, 0x00, 0x00, 0x00, 0x00, 0x32, 0x32, 0x31 // aid
+		//		};
+
+		//byte[] SELECT = { (byte) 0x00, (byte) 0xA4, (byte) 0x00, (byte) 0x00, (byte) 0x00 };
+		byte[] SELECT = { (byte) 0x00, (byte) 0xA4, (byte) 0x00, (byte) 0x00, (byte) 0x02, (byte) 0x3f, (byte) 0x00 };
+
+		byte[] resp = null;
+		try {
+			resp = connection.exchangeData(SELECT);
+		} catch (Exception e) {
+			log.println("could not send apdu: " + e.toString());
+		}
+
+		// this is not working yet :-( the card should respond with
+		// 90 00, yet it responds with 6a 7a
+		
+		if (resp != null) {
+			log.println("returned " + resp.length + " bytes");
+			for (int i = 0; i < resp.length; i++)
+				log.print(resp[i] + " ");
+			log.println("<end>");
+		}
+
+		try {
+			connection.close();
+		} catch (IOException ignored) {
+		}
+		log.println("closed connection");
 	}
 
 	public void openUSBConnection() {
